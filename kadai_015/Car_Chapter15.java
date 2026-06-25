4f627a1300ffbf8f9eb0e1a15acfa3425ff12985
ryohirano <111448052+ryohirano@users.noreply.github.com>
2026-06-25 22:38:53 +0900

    kadai_015

--- kadai_015/Car_Chapter15.java
+++ kadai_015/Car_Chapter15.java
@@ -1,7 +1,6 @@
-package text.kadai_015;
+package kadai_015;
 
 public class Car_Chapter15 {
-	
 	//フィールド内部データ
 	private int gear = 1;//ギア
 	private int speed = 10;//スピード
@@ -9,13 +8,14 @@ public class Car_Chapter15 {
 	//ギアチェンジすると速度も変わる
 	public void changeGear(int afterGear) {
 		this.gear = afterGear;
-		this.speed =this.gear * 10;//ギアに応じて速度変化
-		
+		this.speed = this.gear * 10;//ギアに応じて速度変化
+
 		System.out.println("ギアを" + gear + "に変更しました");
 		System.out.println("現在の速度は" + speed + "km/hです");
 	}
+
 	//走行メソッド
 	public void run() {
 		System.out.println("走行中... 速度は" + speed + "km/h");
 	}
-	}
+}
