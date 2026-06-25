2cbe4974ac16d449c67588db2b4e3175d0ca7e1d
ryohirano <111448052+ryohirano@users.noreply.github.com>
2026-06-25 22:08:24 +0900

    kadai_015

---
+++ kadai_015/CarExec_Chapter15.java
@@ -0,0 +1,19 @@
+package text.kadai_015;
+
+		
+public class CarExec_Chapter15 {
+	
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		//carクラスをインスタンス化
+		Car_Chapter15 myCar = new Car_Chapter15();
+		
+		myCar.run(); //初期速度で走行
+		myCar.changeGear(3); //ギアチェンジ
+		myCar.run(); //ギアチェンジ後の速度で走行
+		
+
+
+	}
+
+}
