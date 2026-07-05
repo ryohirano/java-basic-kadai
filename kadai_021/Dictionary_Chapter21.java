af079a4ce37c1b31422fe04678fedb776c766ce3
ryohirano <111448052+ryohirano@users.noreply.github.com>
2026-07-06 01:26:28 +0900

    kadai_21追加

---
+++ kadai_021/Dictionary_Chapter21.java
@@ -0,0 +1,35 @@
+package kadai_021;
+import java.util.HashMap;
+public class Dictionary_Chapter21 {
+	private final HashMap<String,String> map;
+
+	public Dictionary_Chapter21() {
+		map = new HashMap<>();
+		
+		//辞書に単語を登録
+		map.put("apple", "りんご");
+	    map.put("peach", "桃");
+	    map.put("banana", "バナナ");
+	    map.put("lemon", "レモン");
+	    map.put("pear", "梨");
+	    map.put("kiwi", "キウィ");
+	    map.put("strawberry", "いちご");
+	    map.put("grape", "ぶどう");
+	    map.put("muscat", "マスカット");
+	    map.put("cherry", "さくらんぼ");
+		
+	}
+
+	//単語が存在するか判定
+	public boolean exists(String word) {
+		return map.containsKey(word);
+	}
+
+	//意味を取得
+	public String search(String word) {
+		return map.get(word);
+	}
+	}
+	
+
+
