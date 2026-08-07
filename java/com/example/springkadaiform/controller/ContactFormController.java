03cad900d288aa3b0ac0b869cbe10aa529eba5c0
ryohirano <111448052+ryohirano@users.noreply.github.com>
2026-08-07 09:20:48 +0900

    springboot_basic_kadai 3追加

--- java/com/example/springkadaiform/controller/ContactFormController.java
+++ java/com/example/springkadaiform/controller/ContactFormController.java
@@ -1,5 +1,44 @@
 package com.example.springkadaiform.controller;
 
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.springkadaiform.form.ContactForm;
+import jakarta.validation.Valid;
+
+@Controller
 public class ContactFormController {
+	//フォーム画面の表示
+	@GetMapping("/form")
+     public String showForm(Model model) {
+
+	//すでに上書きが存在する場合は上書きしない
+		if(!model.containsAttribute("contactForm")) {
+			model.addAttribute("contactForm", new ContactForm());
+		}
+
+		return "contactFormView";
+}
+	//確認画面の表示
+	@PostMapping("/confirm")
+	public String confirm(
+			@Valid ContactForm contactForm,
+			BindingResult bindingResult,
+			Model model) {
+
+		//バリデーションNG　→　フォームへ戻す
+		if(bindingResult.hasErrors()) {
+			model.addAttribute("contactForm", contactForm);
+			return "contactFormView";
+		}
+		//バリデーションOK　→　確認画面へ
+		model.addAttribute("contactForm", contactForm);
+		return "confirmView";
+	}
 
 }
