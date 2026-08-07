413317c61065d6b13a2b29e449186c2e53bde9e2
ryohirano <111448052+ryohirano@users.noreply.github.com>
2026-08-07 15:08:55 +0900

    spring_kadai_form

--- spring_kadai_form/src/main/java/com/example/springkadaiform/form/ContactForm.java
+++ spring_kadai_form/src/main/java/com/example/springkadaiform/form/ContactForm.java
@@ -7,35 +7,16 @@ import lombok.Data;
 @Data
 public class ContactForm {
      //ユーザー名
+	@NotBlank(message ="お名前を入力してください")
 	private String name;
 
 	//メールアドレス
+	@NotBlank(message ="メールアドレスを入力してください")
+	@Email(message ="メールアドレスの形式で入力してください")
 	private String email;
 
 	//お問い合わせ内容
+	@NotBlank(message = "お問い合わせ内容を入力してください")
 	private String message;
 
-	//getter/setter
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
 }
